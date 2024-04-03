4f950bd882cd354fba0030dcf0bd824810beb9a1
varshid <44539404+varshid@users.noreply.github.com>
2024-04-02 20:18:07 -0500

    Fixed the DB connection stuff

---
+++ src/main/java/com/example/demo/model/Administrator.java
@@ -0,0 +1,69 @@
+package com.utdesign.if_api;
+
+import java.util.Random;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class Administrator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String adminID;
+    private String adminUser;
+    private String adminPass;
+
+    //Constructors
+    public Administrator(){}
+
+    public Administrator(String adminUser, String adminPass){
+        this.adminID = generateAdminID();
+        this.adminUser = adminUser;
+        this.adminPass = adminPass;
+    }
+
+    //Getters
+    public String getAdminID(){
+        return adminID; 
+    }
+
+    public String getAdminUser(){
+        return adminUser;
+    }
+
+    public String getAdminPass(){
+        return adminPass;
+    }
+
+    //Setters
+    public void setAdminID(String adminID){
+        this.adminID = adminID;
+    }
+
+    public void setAdminUser(String adminUser){
+        this.adminUser = adminUser;
+    }
+
+    public void setAdminPass(String adminPass){
+        this.adminPass = adminPass;
+    }
+
+    //Generate random Administrator ID for use as a primary key
+    public static String generateAdminID(){
+        StringBuilder temp = new StringBuilder();
+        String idCharSet = "0123456789";
+        Random rand = new Random();
+        int randIndex;
+
+        for(int i = 0; i < 9; i++){
+            randIndex = rand.nextInt(10);
+            temp.append(idCharSet.charAt(randIndex));
+        }
+
+        String id = temp.toString();
+
+        return id;
+    }
+}
---
+++ src/main/java/com/example/demo/model/Image.java
@@ -0,0 +1,39 @@
+package com.utdesign.if_api;
+
+import java.sql.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int imageID;
+    private Blob imageData;
+    private int imageSize;
+
+    //Constructors
+    public Image(){}
+
+    public Image(int imageID, Blob imageData, int imageSize){
+        this.imageID = imageID;
+        this.imageData =imageData;
+        this. imageSize = imageSize;
+    }
+
+    //Getters
+    public int getImageID(){return imageID;}
+
+    public Blob getImageData(){return imageData;}
+
+    public int imageSize(){return imageSize;}
+
+    //Setters
+    public void setImageID(int imageID){this.imageID = imageID;}
+
+    public void setImageData(Blob imageData){this.imageData = imageData;}
+
+    public void setImageSize(int imageSize){this.imageSize = imageSize;}
+}
